b91d1a3391f0059de6d5e4ce3031e25fe1a8c5ec
anete bremane <anetebremane@gmail.com>
2022-03-22 17:47:18 +0200

    Day 2 tasks

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,80 @@
-public class Main {
-    public static void main(String[] args) {
-        byte b;
-        short sh;
-        int i;
-        long l;
-        float f;
-        double doubl;
-        boolean bool;
-        char c;
+import java.util.Scanner;
 
-        b = 1;
-        sh = 22;
-        i = 3;
-        l = 4567;
-        f = 5.5f;
-        doubl = 6.53778;
-        bool = true;
-        c = 7;
-        if(i<2) System.out.println(c);
-        System.out.println(l);
+public class Main {
+    static Scanner skeneris = new Scanner(System.in);
+    public static void main(String[] args) {
+        Main.intN();
+    Main.compare();
+    Main.bool();
+    Main.wholeN();
+    Main.wholeNum();
+
+
+
+
+
+    skeneris.close();
+    }
+
+    public static void compare() {
+
+        System.out.println("input two numbers!");
+        int a = skeneris.nextInt();
+        int b = skeneris.nextInt();
+        System.out.println("is " + a + " equal to " + b + " ?");
+        System.out.println(a == b);
+
+        System.out.println("is " + a + " less than " + b);
+        System.out.println(a < b);
+
+        System.out.println("is " + a + " less or equal to " + b);
+        System.out.println(a <= b);
+
+        System.out.println("is " + a + " greater than " + b);
+        System.out.println(a > b);
+
+        System.out.println("is " + a + " greater or equal " + b);
+        System.out.println(a >= b);
+        }
+    public static void bool() {
+        System.out.println("input two boolean values!");
+        boolean b1 = skeneris.nextBoolean();
+        boolean b2 = skeneris.nextBoolean();
+
+        System.out.println("Is b1 and b2 equal?");
+        System.out.println(b1 == b2);
+    }
+    public static void wholeN() {
+        System.out.println("input whole number!");
+        int a = skeneris.nextInt();
+        System.out.println("Is number even?");
+        System.out.println(a % 2 == 0);
+
+    }
+    public static void wholeNum(){
+        System.out.println("input whole number!");
+        int a = skeneris.nextInt();
+        System.out.println("Is number odd?");
+        System.out.println(a % 2 != 0);
+    }
+    public static void boolN(){
+        System.out.println("input boolean value");
+        boolean a = skeneris.nextBoolean();
+        System.out.println("Opposite of " + a + "is" + !a);
+    }
+    public static void intN(){
+        System.out.println("input two whole numbers");
+        int a = skeneris.nextInt();
+        int b = skeneris.nextInt();
+        if (a == b){
+            System.out.println("true");
+        }
+        else if (a < 0 && b > 0) {
+            System.out.println("true");
+        }
+        else if (a > 1001 && b > 1001){
+            System.out.println("true");
+        }
+        else System.out.println("false");
     }
 }
