87a25d44cb3914a6a8897c724e7a55f5ceae862b
anete bremane <anetebremane@gmail.com>
2022-03-22 12:37:10 +0200

    first

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b;
+        short sh;
+        int i;
+        long l;
+        float f;
+        double d;
+        boolean bool;
+        char c;
+
+        b = 1;
+        sh = 22;
+        i = 3;
+        l = 4567;
+        f = 5.5f;
+        d = 6.53778;
+        bool = true;
+        c = 7;
+        System.out.println(l);
+    }
+}
